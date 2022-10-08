ef4dec9f442163d7ca2d30b847959db2a88fcaa6
Jason Florentino <jasonmflorentino@gmail.com>
2022-10-08 18:47:46 -0400

    remove server dep

--- README.md
+++ README.md
@@ -1,5 +1,6 @@
 # 🏥 COVID in York Region
-***A simple Covid dashboard tracking active cases in York Region***
+
+**_A simple Covid dashboard tracking active cases in York Region_**
 
 ---
 
@@ -14,3 +15,12 @@ I wanted some practice making apps and at the same time I wanted to give my pare
 Thanks for visiting,
 
 —Jason (2021-04-12)
+
+## Deprecation Notice
+
+As of Oct 7, 2022 this app is frozen in time.
+The server and database that backed this app will be shutdown, and the data it had is now bundled as part of this repo.
+And since there is no longer a database to update, the daily job that pulled new COVID data from York Region will no longer run.
+For reference, those systems can be found here: https://github.com/jasonflorentino/covid-in-york-region-server
+
+Best Wishes, Jason.
--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React, { useState, useEffect } from 'react';
-import axios from 'axios';
+// import axios from 'axios';
+
+import DailiesData from './data/dailies.json'
 
 import ChartDailies from './components/ChartDailies/ChartDailies';
 import QuickStats from './components/QuickStats/QuickStats';
@@ -16,17 +18,20 @@ const App = () => {
   const [error, setError] = useState(false);
   
   const fetchData = () => {
-    const url = process.env.REACT_APP_API_URL + '/api';
-    axios
-      .get(url)
-      .then(res => {
-        setData(res.data);
-      })
-      .then(() => setLoading(false))
-      .catch(err => {
-        setError(true);
-        console.error(err);
-      })
+    setData(DailiesData)
+    setLoading(false)
+    return;
+    // const url = process.env.REACT_APP_API_URL + '/api';
+    // axios
+    //   .get(url)
+    //   .then(res => {
+    //     setData(res.data);
+    //   })
+    //   .then(() => setLoading(false))
+    //   .catch(err => {
+    //     setError(true);
+    //     console.error(err);
+    //   })
   }
 
   useEffect(() => {
@@ -35,6 +40,9 @@ const App = () => {
 
   return (
     <div className='App'>
+      <h3>
+        This app is no longer active! What follows is how it looked when I shut it down. Best Wishes! —Jason
+      </h3>
       <Header />
       <main className='App__main'>
         {error ? <Error /> :
